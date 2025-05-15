d133a838b3a5988058aed294290057ce08c0a496
nchuy099 <nchihuy099@gmail.com>
2025-05-16 00:12:59 +0700

    add redis pass config

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,11 +1,13 @@
 const Redis = require('ioredis');
 
 const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
-const REDIS_PORT = process.env.REDIS_PORT || 6379; // Lưu ý: PORT là số, không phải string
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD || ''; // lấy từ biến môi trường
 
 const redisClient = new Redis({
     host: REDIS_HOST,
     port: REDIS_PORT,
+    password: REDIS_PASSWORD, // thêm dòng này
 });
 
 redisClient.on('connect', () => console.log('Connected to Redis successfully.'));
